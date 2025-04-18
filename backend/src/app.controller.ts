5f24aca67b37600362daa666cf7365c399c3627d
Dinesh-Abhi <105792851+Dinesh-Abhi@users.noreply.github.com>
2025-04-18 12:09:27 +0530

    feat: add delete user endpoint to AppController

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
 import { SessionContainer } from 'supertokens-node/recipe/session';
 import Multitenancy from 'supertokens-node/recipe/multitenancy';
 import { AppService } from './app.service';
 import { AuthGuard } from './auth/auth.guard';
 import { Session } from './auth/session/session.decorator';
+import { deleteUser } from 'supertokens-node';
+import { ApiParam } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('/hello')
   getHello(): string {
@@ -31,5 +33,16 @@ export class AppController {
   @Get('/tenants')
   async getTenants(): Promise<any> {
     return await Multitenancy.listAllTenants();
+  }
+
+  @Delete('remove-user/:userId')
+  @ApiParam({
+    name: 'userId',
+    description: 'UUID of the user to delete',
+    type: String,
+  })
+  async deleteUserForId(@Param('userId') userId: string): Promise<{ status: string }> {
+    const result = await deleteUser(userId);
+    return result;
   }
 }
